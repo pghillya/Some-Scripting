66ab01ebe92eabe4441de94203f9dd816cf84873
Peter Hillyard <pghillya@ncsu.edu>
2021-12-28 23:24:24 -0500

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
-cd app/
-python script.py
+dpython script.py
 service sendmail start
 
 echo "Your daily market close summary report" | mutt -e "set from='peter hillyard <autoreports@petersReportServer>'"\
- -a "BasicReport.xlsx" -s "Daily Stonks Summary" -- pghillya@ncsu.edu
+ -a BasicReport*.xlsx -s "Daily Stonks Summary" -- pghillya@ncsu.edu
 if [ $? -eq 0 ]
 then
   echo "Successfully sent mail!"
@@ -11,7 +10,7 @@ else
   echo "Mail failed to send, please try again..."
 fi
 
-# Make sure the container has a little extra time to send before it stops
+# Make sure the container has a little extra time to send before it exits
  sleep 20s
 
  

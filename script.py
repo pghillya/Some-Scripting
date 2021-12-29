e337ff0c54b31d3d68de1b714ee0d0b2e0bd6c06
Peter Hillyard <pghillya@ncsu.edu>
2021-12-28 23:02:25 -0500

    added historical data

--- script.py
+++ script.py
@@ -3,8 +3,14 @@ from bs4 import BeautifulSoup
 from datetime import date
 from openpyxl import Workbook
 
-#Constant(s)
+# Constant(s)
 today=date.today()
+cur_month=today.month
+month_ago=today.replace(month=(cur_month-1))
+
+# Reformat Dates for hist data URL Parameters
+today=today.strftime("%m/%d/%Y")
+month_ago=month_ago.strftime("%m/%d/%Y")
 
 # Define the stock symbols that we want to use, eventually with a separate file
 fn = 'symbols.csv'
@@ -38,7 +44,16 @@ with open(fn) as f:
         dividend_and_yield=(results.find("td", attrs={"data-test": "DIVIDEND_AND_YIELD-value"})).text.strip()
         # stock_data=results.find_all("td", class_="Ta(end) Fw(600) Lh(14px)") << one way to get all the values more easily
 
-       
+        # Get the historical data for 1 month
+        h_url=f"https://www.marketwatch.com/investing/stock/{symbol}/downloaddatapartial?startdate={month_ago}%2000:00:00&enddate={today}%2023:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false"
+        h_r=requests.get(h_url)
+
+        # Format
+        h_data = h_r.content.decode('utf-8')
+        h = csv.reader(h_data.splitlines(), delimiter=',')
+        history = list(h)
+
+        # Create the worksheet
         ws=wb.create_sheet(f"{symbol}")
 
         # Write the columns (cells can be fixed)
@@ -59,6 +74,12 @@ with open(fn) as f:
         ws.cell(row=2, column=6, value=dividend_and_yield)
         ws.cell(row=2, column=7, value=today)
 
+        # ------------Last 1 Month------------ #
+        ws.append([])
+        ws.append([])
 
-wb.save('BasicReport.xlsx')
-print(wb.sheetnames)
+        for row in history:
+            ws.append(row)
+
+wb.save(f"BasicReport-{date.today()}.xlsx")
+print("created worksheets: " + str(wb.sheetnames))
